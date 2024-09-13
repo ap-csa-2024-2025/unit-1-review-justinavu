bad14ea63ebf769aaf23f6be14d9811952c25331
justinavu <justinvu2008@gmail.com>
2024-09-13 09:58:21 -0700

    sync

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,32 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String courseName = "AP Chemistry";
+      System.out.println("Course Name: " + courseName);
+    int avgTime = 60;
+      System.out.println("Average time spent in a week for this course(mins): " + avgTime);
+
+    int hw1 = 75;
+    int hw2 = 99;
+    int hw3 = 90;
+    int hw4 = 100;
+    System.out.println("Homework scores: " + hw1 + "," + hw2 + "," + hw3 + "," + hw4);
+
+    double quiz1 = 89.2;
+    double quiz2 = 98.1;
+    System.out.println("Quiz scores: " + quiz1 + "," + quiz2); 
+
+    double Final = 87.58; 
+    System.out.println("Final Exam score :" + Final);
+
+    System.out.println("Weekly time spent: " + (135/60) +"hrs" + (135%60) + "mins");
+    System.out.println("Average HW grade: " + ((double) (hw1 + hw2 + hw3 + hw4))/4);
+    System.out.println("Average quiz grade: " + (quiz1 + quiz2)/4);
+
+    double finalGrade = (((double) (hw1 + hw2 + hw3 + hw4))/4*(35.0/100)) + ((quiz1 + quiz2)/4*(15.0/100)) + 
+    (Final/4*(15.0/100)); 
+    System.out.println("Final grade: " + finalGrade);
+
+
   }
 }
